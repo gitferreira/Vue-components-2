19e7711b51ef4adb61dad1fe7770e20c40eebb1b
gitferreira <iniferre96@gmail.com>
2021-11-18 12:19:37 +0100

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>My friends</h2>
+  <ul>
+<friend-contact></friend-contact>
+<friend-contact></friend-contact>
+  </ul>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+    FriendContacts: [
+        {
+          id: "Manu",
+          name: "Manuel Ruiz",
+          phone: "944609878",
+          email: "banana@gmail.com",
+        },
+        {
+          id: "Jules",
+          name: "Jules Roy",
+          phone: "944607438478",
+          email: "banana1@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>Phone: {{ friend.phone }}</li>
+      <li>Email: {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "Manu",
+        name: "Manuel Ruiz",
+        phone: "944609878",
+        email: "banana@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
